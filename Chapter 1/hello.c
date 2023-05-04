ecbecb0dbfc82f7775e659bc29b86153bfc5464c
ANIL80531 <anil80531@gmail.com>
2023-05-04 22:23:57 +0530

    clang@termux

--- Chapter 1/hello.c
+++ Chapter 1/hello.c
@@ -1,4 +1,23 @@
-#include<stdio.h>
+#include<stdio.h>	//	code/library which other wrote for our use.
 int main(){
-	printf("Hello, World!\n");
+	// main is a special function every program start executing from main.
+	printf("Hello, World!\n");	// printf is function from stdio.h library.
+	//	arguments are used to communicate data between functions.
+	//	arguments are surrounded by parenthesis after function.
+	//	empty parenthesis after a function means it accepts no argument.
+	//	\n is new line character.
+	//	the body of function which contains statements is enclosed in {}
+	//	A function is called by using its name and followed by arguments in parenthesis.
+	//	Characters in double quotes are string constant.
+	
+
+
+
+
+
+
+
+	putchar('\n');
+	putchar('\n');
+	putchar('\n');
 }
