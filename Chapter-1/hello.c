6fc0d2b97a6fa5bbf07952318cc7c2629c37cb01
ANIL80531 <anil80531@gmail.com>
2023-05-21 14:20:13 +0000

    vscode server on fedora@termux

--- Chapter-1/hello.c
+++ Chapter-1/hello.c
@@ -1,27 +1,4 @@
-#include<stdio.h>	//	code/library which other wrote for our use.
+#include<stdio.h>
 int main(){
-	putchar('\n');
-	// main is a special function every program start executing from main.
-	printf("Hello, World!\n");	// printf is function from stdio.h library.
-	//	arguments are used to communicate data between functions.
-	//	arguments are surrounded by parenthesis after function.
-	//	empty parenthesis after a function means it accepts no argument.
-	//	\n is new line character.
-	//	the body of function which contains statements is enclosed in {}
-	//	A function is called by using its name and followed by arguments in parenthesis.
-	//	Characters in double quotes are string constant.
-	printf("Hello,");
-	printf(" World!");
-	printf("\n");
-	// produces identical results
-	
-
-
-
-
-
-
-	putchar('\n');
-	putchar('\n');
-	putchar('\n');
+	printf("Hello, World!\n");
 }
